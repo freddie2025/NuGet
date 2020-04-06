80385a1ad6db9b5670cd8c12c4d42d7b20d3e6ba
Fred James <fredjsam@live.co.uk>
2020-04-06 15:15:04 +0100

    Broken Build Fix

--- NuGetPackageDemo/ConsoleUI/Program.cs
+++ NuGetPackageDemo/ConsoleUI/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleUI
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine($"The sum of 5 and 2 is { Calculator.Add(2,7) }");
+			//Console.WriteLine($"The sum of 5 and 2 is { Calculator.Add(2,7) }");
 		}
 	}
 }
